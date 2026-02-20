bb5bc813891cc92d59ae9bce170eefafde0997d8
Ubuntu <ubuntu@ip-172-31-44-96.ap-south-1.compute.internal>
2026-02-20 07:58:56 +0000

    Added capstone node app files

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from DevOps Capstone Project!');
+});
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('App running on port 3000');
+});
+
+
+
+
+
+
+
+
+
+
+
